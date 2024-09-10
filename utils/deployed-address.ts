4580c0a162c5d86731efdf28cb8e8066beae9695
paster212 <arthawijayaramadan@gmail.com>
2024-09-10 18:53:15 +0700

    first commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x6A714E9eb5F4CBCD4E5CFa3aabb4fcb8998E06DD'
+export const ProxyAdmin = '0x0Db85976E6274b64B985daBA0b138fcE893a7673'
+export const SWTRImplementation = '0xDfa9b8fBCf6ffFe397B0992F413F6f49361Ae669'
+    
